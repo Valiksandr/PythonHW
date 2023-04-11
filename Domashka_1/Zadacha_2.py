ab6b2b267078579a868aa79b7333a545d7d5624e
Alex <valuevsky@gmail.com>
2023-04-11 21:03:13 +0300

    zadacha 4 - done

--- Domashka_1/Zadacha_2.py
+++ Domashka_1/Zadacha_2.py
@@ -1,7 +1,2 @@
 
 
-username = input('Введите имя: ')
-if username == 'Sasha':
-    print ('Fucking YEA!!!')
-else:
-    print ('I am sad!')
---
+++ Domashka_1/Zadacha_4.py
@@ -0,0 +1,6 @@
+vsego_jur=int(input("Всего сделано журавликов: "))
+parni=vsego_jur//6
+kate=parni*4
+print(f"Петя сделал -",parni,"журавлик(а/ов)")
+print(f"Серега сделал -",parni,"журавлик(а/ов)")
+print(f"Катя сделала -",kate,"журавлик(а/ов)")

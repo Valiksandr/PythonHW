a4a442bef6a5f71d40c32a6a6598d3ab4ef3b71e
Alex <valuevsky@gmail.com>
2023-04-21 15:00:47 +0300

    zadacha 28 - dodelal

--- Domashka_5/Zadacha_28.py
+++ Domashka_5/Zadacha_28.py
@@ -1,10 +1,10 @@
 def sum(a, b):
     if b != 0:
-        a += 1
-        b -= 1
-        sum(a, b)
+        return sum(a + 1, b-1)
     else:
         print(a)
+
+
 a = int(input('Введите первое число: '))
 b = int(input('Введите второе число: '))
 sum(a, b)
